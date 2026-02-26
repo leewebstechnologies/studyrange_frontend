9b58f7578a1001a6e6ddb91810fc515de09bd648
leewebstechnologies.ali <leewebstechnologies.aliuajibade317@gmail.com>
2026-02-26 09:56:42 -0800

    add top and left cards

--- app/components/hero/Hero.tsx
+++ app/components/hero/Hero.tsx
@@ -26,34 +26,58 @@ const Hero = () => {
           </span>
         </div>
         <div className={styles.heroCardsLeft}>
-          <div className={styles.heroCardLabel}>
-            <Image
-              alt="star"
-              width={18}
-              height={24}
-              src="/images/hero/star.png"
-            />
-            <span>Your Journey to UK Education Starts Here</span>
-          </div>
-          <h4 className={styles.heroCardTopText}>
-            Transform Your Future with Expert Education Consultancy
-          </h4>
-          <p className={styles.heroCardBottomText}>
-            Since 2019, Studyrange Consult has guided thousands of students to
-            their dream UK universities. Experience personalized counselling,
-            comprehensive application support, and expert visa guidance.
-          </p>
-          <div className={styles.heroCardButtonContainer}>
-            <div className={styles.studyMatch}>
+          <div className={styles.heroCardsLeftContents}>
+            <div className={styles.heroCardLabel}>
               <Image
-              className={styles.question}
-              src="/images/hero/i.png"
-              alt="star"
-              width={16}
-              height={20}
+                alt="star"
+                width={18}
+                height={24}
+                src="/images/hero/star.png"
               />
-              <span>Try Study Match AI</span>
+              <span>Your Journey to UK Education Starts Here</span>
             </div>
+            <h4 className={styles.heroCardTopText}>
+              Transform Your Future with Expert Education Consultancy
+            </h4>
+            <p className={styles.heroCardBottomText}>
+              Since 2019, Studyrange Consult has guided thousands of students to
+              their dream UK universities. Experience personalized counselling,
+              comprehensive application support, and expert visa guidance.
+            </p>
+            <div className={styles.actions}>
+              <button className={styles.primary}>
+                <Image
+                  className={styles.question}
+                  src="/images/hero/i.png"
+                  alt="star"
+                  width={16}
+                  height={20}
+                />
+                <span className={styles.primaryText}>Try Study Match AI</span>
+              </button>
+              <button className={styles.secondary}>
+                <Image
+                  className={styles.question}
+                  src="/images/hero/consult.png"
+                  alt="star"
+                  width={16}
+                  height={20}
+                />
+                <span className={styles.secondaryText}>
+                  Book Free Consultation
+                </span>
+              </button>
+            </div>
+            <button className={styles.apply}>
+              <Image
+                className={styles.question}
+                src="/images/hero/apply.png"
+                alt="star"
+                width={16}
+                height={20}
+              />
+              <span>Apply Now!</span>
+            </button>
           </div>
         </div>
       </div>
