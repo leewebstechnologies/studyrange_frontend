76f533ef510a2539812f980ee2af7d6f44acac2f
leewebstechnologies.ali <leewebstechnologies.aliuajibade317@gmail.com>
2026-02-27 03:22:30 -0800

    add heroCardsRight

--- app/components/hero/Hero.tsx
+++ app/components/hero/Hero.tsx
@@ -25,84 +25,94 @@ const Hero = () => {
             Trusted by 2,500+ Students
           </span>
         </div>
-        <div className={styles.heroCardsLeft}>
-          <div className={styles.heroCardsLeftContents}>
-            <div className={styles.heroCardLabel}>
-              <Image
-                alt="star"
-                width={18}
-                height={24}
-                src="/images/hero/star.png"
-              />
-              <span>Your Journey to UK Education Starts Here</span>
-            </div>
-            <h4 className={styles.heroCardTopText}>
-              Transform Your Future with Expert Education Consultancy
-            </h4>
-            <p className={styles.heroCardBottomText}>
-              Since 2019, Studyrange Consult has guided thousands of students to
-              their dream UK universities. Experience personalized counselling,
-              comprehensive application support, and expert visa guidance.
-            </p>
-            <div className={styles.actions}>
-              <button className={styles.primary}>
+        <div className={styles.heroCardsRow}>
+          <div className={styles.heroCardsLeft}>
+            <div className={styles.heroCardsLeftContents}>
+              <div className={styles.heroCardLabel}>
+                <Image
+                  alt="star"
+                  width={18}
+                  height={24}
+                  src="/images/hero/star.png"
+                />
+                <span>Your Journey to UK Education Starts Here</span>
+              </div>
+              <h4 className={styles.heroCardTopText}>
+                Transform Your Future with Expert Education Consultancy
+              </h4>
+              <p className={styles.heroCardBottomText}>
+                Since 2019, Studyrange Consult has guided thousands of students
+                to their dream UK universities. Experience personalized
+                counselling, comprehensive application support, and expert visa
+                guidance.
+              </p>
+              <div className={styles.actions}>
+                <button className={styles.primary}>
+                  <Image
+                    className={styles.question}
+                    src="/images/hero/i.png"
+                    alt="star"
+                    width={16}
+                    height={20}
+                  />
+                  <span className={styles.primaryText}>Try Study Match AI</span>
+                </button>
+                <button className={styles.secondary}>
+                  <Image
+                    className={styles.question}
+                    src="/images/hero/consult.png"
+                    alt="star"
+                    width={16}
+                    height={20}
+                  />
+                  <span className={styles.secondaryText}>
+                    Book Free Consultation
+                  </span>
+                </button>
+              </div>
+              <button className={styles.apply}>
                 <Image
                   className={styles.question}
-                  src="/images/hero/i.png"
+                  src="/images/hero/apply.png"
                   alt="star"
                   width={16}
                   height={20}
                 />
-                <span className={styles.primaryText}>Try Study Match AI</span>
-              </button>
-              <button className={styles.secondary}>
-                <Image
-                  className={styles.question}
-                  src="/images/hero/consult.png"
-                  alt="star"
-                  width={16}
-                  height={20}
-                />
-                <span className={styles.secondaryText}>
-                  Book Free Consultation
-                </span>
+                <span>Apply Now!</span>
               </button>
             </div>
-            <button className={styles.apply}>
-              <Image
-                className={styles.question}
-                src="/images/hero/apply.png"
-                alt="star"
-                width={16}
-                height={20}
-              />
-              <span>Apply Now!</span>
-            </button>
           </div>
-        </div>
 
-        <div className={styles.heroCardsRight}>
-          <Image
-            className={styles.whatsapp}
-            src="/images/hero/whatsapp.png"
-            alt="star"
-            width={62}
-            height={62}
-          />
-          <div className={styles.rating}>
-            <div className={styles.ratingItems}>
-              <Image
-                className={styles.rating}
-                src="/images/hero/rating.png"
-                alt="star"
-                width={64}
-                height={64}
-              />
-              <div className={styles.ratingText}>
-                <span className={styles.ratingNumber}>4.9</span>
-                <p className={styles.studentRating}>Student Rating</p>
+          <div className={styles.heroCardsRight}>
+            <Image
+              className={styles.whatsapp}
+              src="/images/hero/whatsapp.png"
+              alt="star"
+              width={62}
+              height={62}
+            />
+            <div className={styles.rating}>
+              <div className={styles.ratingItems}>
+                <Image
+                  className={styles.rating}
+                  src="/images/hero/rating.png"
+                  alt="star"
+                  width={64}
+                  height={64}
+                />
+                <div className={styles.ratingText}>
+                  <span className={styles.ratingNumber}>4.9</span>
+                  <p className={styles.studentRating}>Student Rating</p>
+                </div>
               </div>
             </div>
+            <Image
+              className={styles.consult}
+              src="/images/hero/consult.png"
+              alt="star"
+              width={14}
+              height={20}
+            />
           </div>
         </div>
       </div>
